8e4bcdc11fbc36e57b8036ed35a2aed120df755f
vanessa <vanessa.bert311@gmail.com>
2023-11-12 18:36:12 -0300

    base project fintech

---
+++ src/main/java/br/com/fintech/models/DespesaModel.java
@@ -0,0 +1,67 @@
+package br.com.fintech.models;
+
+import java.util.Calendar;
+
+public class DespesaModel{
+	private int id;
+	private String tipo;
+	private double valor;
+	private Calendar dataCriacao;
+	private int usuarioID;
+	
+	public DespesaModel() {
+		super();
+	}
+	
+	public DespesaModel(int id, String tipo, double valor, Calendar dataCriacao, int usuarioID) {
+		super();
+		this.id = id;
+		this.tipo = tipo;
+		this.valor = valor;
+		this.dataCriacao = dataCriacao;
+		this.usuarioID = usuarioID;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public Calendar getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(Calendar dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public int getUsuarioID() {
+		return usuarioID;
+	}
+
+	public void setUsuarioID(int usuarioID) {
+		this.usuarioID = usuarioID;
+	}
+	
+	
+	
+}
